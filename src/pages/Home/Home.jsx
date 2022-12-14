e113230aeb3e6a58cfe86ce96c454a32196eaf8e
KautTanya <kaut.tanya.s@gmail.com>
2022-12-15 01:40:54 +0200

    finish

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -16,10 +16,7 @@ const styles = {
     return (
       <div style={styles.container}>
         <h1 style={styles.title}>
-          Contacts book welcome page{' '}
-          <span role="img" aria-label="Greeting icon">
-            💁‍♀️
-          </span>
+          Welcome to the PHONEBOOK!))))
         </h1>
       </div>
     );
